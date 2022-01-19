c451e4cac5d8c5706b8495ae9e5f3e0647798eca
Orest Dorokhin <orest_dorokhin@epam.com>
2022-01-19 20:13:51 +0200

    app.js was extended the second time

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send("don't panic"));
+app.get('/', (req, res) => res.send("don't panic and carry a towel"));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
